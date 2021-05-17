2fe13388b7c142a9aabcbc42d7d870025b9749b7
codrelphi <chancerel.codjovi@gmail.com>
2021-05-17 09:20:24 +0100

    ajouter TestInfiniteDoWhile.java

---
+++ TestInfiniteDoWhile.java
@@ -0,0 +1,16 @@
+/* objectif: tester la boucle do...while
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 17.05.2021
+ *
+ */
+
+
+public class TestInfiniteDoWhile {
+
+	public static void main (String[] args) {
+
+		byte compteur = 5;
+
+		do System.out.println("Ooh: " + compteur); while (compteur >=0);
+	}
+}
