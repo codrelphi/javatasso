795aef62080247e3db70b814c4dc9c0c52acaaa7
codrelphi <chancerel.codjovi@gmail.com>
2021-05-20 01:37:05 +0100

    TestForParticularity.java: for(;;) - ca compile, boucle infini

---
+++ TestForParticularity.java
@@ -0,0 +1,21 @@
+/* objectif: tester quelques particularités de for
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 20.05.2021
+ *
+ */
+
+
+import java.util.Scanner;
+
+
+public class TestForParticularity {
+
+	public static void main (String[] args) {
+
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print("Entrez une valeur: ");
+		byte valeur = lectureClavier.nextByte();
+
+		for (;;) System.out.println("Hello"); // ca compile
+	}
+}
