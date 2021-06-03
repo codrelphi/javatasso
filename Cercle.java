1d8a9bd9bcb649a5711438dda750706023c5d5cf
codrelphi <chancerel.codjovi@gmail.com>
2021-06-03 10:58:17 +0100

    Cercle.java - ajout de la méthode créer()

--- Cercle.java
+++ Cercle.java
@@ -1,14 +1,28 @@
 /* objectif: définir l'objet Cercle (caractéristiques et comportements)
  * auteur: Chancerel Codjovi (aka codrelphi)
  * date: 02.06.2021
+ * update: 03.06.2021
  *
  */
 
+import java.util.Scanner;
 
 public class Cercle {
 
 	public int x, y; // position au centre du cercle
 	public int r; // rayon du cercle
+	public static int nombre; // nombre de cercle 
+
+	public void créer() {
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print(" Position en x : ");
+		x = lectureClavier.nextInt();
+		System.out.print(" Position en y : ");
+		y = lectureClavier.nextInt();
+		System.out.print(" Rayon : ");
+		r = lectureClavier.nextInt();
+		nombre++;
+	}
 
 	public void afficher() {
 		System.out.println("Cercle centré en " + x + ", " + y);
