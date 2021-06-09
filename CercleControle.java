15cbb124589da032df953da4992a330a5b82dd73
codrelphi <chancerel.codjovi@gmail.com>
2021-06-09 12:53:40 +0100

    CercleControle.java supprimer la méthode main

--- CercleControle.java
+++ CercleControle.java
@@ -8,31 +8,29 @@ import java.util.Scanner;
 
 public class CercleControle {
 
-	public static void main (String[] args) {
-		private int x, y, r; // position du centre et rayon
+	private int x, y, r; // position du centre et rayon
 
-		public void creer() {
-			Scanner lectureClavier = new Scanner(System.in);
-			System.out.print(" Position en x : ");
-			x = lectureClavier.nextInt();
-			System.out.print(" Position en y : ");
-			y = lectureClavier.nextInt();
-			do {
-				System.out.print(" Rayon              :");
-				r = lectureClavier.nextInt();
-			} while (r<0 || r>600);
-		}
-
-		public void afficher() { //Affichage des données de la classe
-			System.out.println(" Centre en " + x + ", " + y);
-			System.out.println(" Rayon : " + r);
-		}
-
-		public void agrandir(int nr) {
-			if (r + nr < 0) r = 0;
-			else if (r + nr > 600) r = 600;
-			else r += nr;
-		}
-
+	public void creer() {
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print(" Position en x : ");
+		x = lectureClavier.nextInt();
+		System.out.print(" Position en y : ");
+		y = lectureClavier.nextInt();
+		do {
+			System.out.print(" Rayon              :");
+			r = lectureClavier.nextInt();
+		} while (r<0 || r>600);
 	}
+
+	public void afficher() { //Affichage des données de la classe
+		System.out.println(" Centre en " + x + ", " + y);
+		System.out.println(" Rayon : " + r);
+	}
+
+	public void agrandir(int nr) {
+		if (r + nr < 0) r = 0;
+		else if (r + nr > 600) r = 600;
+		else r += nr;
+	}
+
 }
---
+++ FaireDesCerclesControles.java
@@ -0,0 +1,24 @@
+/* objectif: pour utiliser la classe CercleControle
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 09.06.2021
+ *
+ */
+
+import java.util.Scanner;
+
+public class FaireDesCerclesControles {
+
+	public static void main (String[] args) {
+
+		Scanner lectureClavier = new Scanner(System.in);
+		CercleControle A = new CercleControle();
+		A.creer();
+		A.afficher();
+		System.out.print(" Entrez une valeur d'agrandissement : ");
+		int plus = lectureClavier.nextInt();
+		A.agrandir(plus);
+		System.out.println("Après agrandissement : ");
+		A.afficher();
+	}
+
+}
