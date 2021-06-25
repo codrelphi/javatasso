a2b0af4eb3ebabdd930fc990197130fa2ca8c497
codrelphi <chancerel.codjovi@gmail.com>
2021-06-25 22:43:14 +0100

    ajout de Etudiant.java, TestEtudiant.java

---
+++ ch9/Etudiant.java
@@ -0,0 +1,54 @@
+/* objectif: réaliser le classement par moyenne d'une classe d'étudiants
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 25.06.2021
+ *
+ */
+
+import java.util.Scanner;
+
+
+public class Etudiant {
+
+	private String nom, prenom;
+	private double [] notes;
+	private double moyenne;
+
+	public Etudiant() {
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print("Entrez le nom : ");
+		nom = lectureClavier.next();
+		System.out.print("Entrez le prénom : ");
+		prenom = lectureClavier.next();
+		System.out.print("Combien de notes pour l'étudiant ");
+		System.out.print(prenom + " " + nom + " : ");
+		var nombre = lectureClavier.nextByte();
+		notes = new double[nombre];
+		for (var i = 0; i < notes.length; i++) {
+			System.out.print("Entrez la note n°" + (i+1) + " : ");
+			notes[i] = lectureClavier.nextInt();
+		}
+		moyenne = calculMoyenne();
+	}
+
+	public double calculMoyenne() {
+		double somme = 0.0;
+		for (double note : notes) somme += note;
+		return somme / notes.length;
+	}
+
+	public double quelleMoyenne() {
+		return moyenne;
+	}
+
+	public void afficherUnEtudiant() {
+
+		System.out.print("Les notes de " + prenom + " " + nom + " sont : ");
+		for (var note : notes) 
+			System.out.print(" " + note);
+		System.out.println();
+		System.out.println("sa moyenne vaut : " + this.moyenne);
+
+	}
+
+
+}
---
+++ ch9/TestEtudiant.java
@@ -0,0 +1,15 @@
+/* objectif: tester la classe Etudiant
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 25.06.2021
+ *
+ */
+
+public class TestEtudiant {
+
+	public static void main(String [] args) {
+
+		Etudiant etudiant = new Etudiant();
+		etudiant.afficherUnEtudiant();
+
+	}
+}
