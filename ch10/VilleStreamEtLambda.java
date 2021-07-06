38a5569ec2038a322e15eb20e27726264307d701
codrelphi <chancerel.codjovi@gmail.com>
2021-07-06 11:22:25 +0100

    VilleStreamEtLambda.java: implémenter le nombre de villes débutant par un caractere

--- ch10/VilleStreamEtLambda.java
+++ ch10/VilleStreamEtLambda.java
@@ -6,11 +6,13 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class VilleStreamEtLambda {
 
 	static short nbVillesSup6 = 0;
 	static short nbVillesInf6 = 0;
+	static short nbVillesDebutantParCaractere = 0;
 
 	public static void main(String [] args) {
 
@@ -29,27 +31,41 @@ public class VilleStreamEtLambda {
 		villes.add("Meknès");
 		villes.add("Benguérir");
 		var taille = villes.size();
-		System.out.println("Villes (Taille: " + taille + "): " + villes);
-		System.out.println();
+		System.out.println("Villes (Taille: " + taille + "): " + villes + "\n");
 		Collections.sort(villes);
-		System.out.println("Ville triée: " + villes);
+		System.out.println("Ville triée: " + villes + "\n");
 
 		villes.stream()
 			.map(ville -> ville.toUpperCase())
 			.forEach(ville -> {
 				if(ville.length() > 6) {
-					System.out.println("Ville: " + ville);
+					System.out.println("Ville : " + ville);
 					nbVillesSup6++;
 				}
 				else {
-					System.out.println("Ville: " + "trop petite !");
+					System.out.println("Ville : " + "trop petite !");
 					nbVillesInf6++;
 				}
 			});
 		
 		System.out.println();
 		System.out.println("Le nombre de ville (Taille > 6) est : " + nbVillesSup6);
-		System.out.println("Le nombre de ville (Taille <=6) est : " + nbVillesInf6);
+		System.out.println("Le nombre de ville (Taille <= 6) est : " + nbVillesInf6);
+
+		// on veut déterminer le nombre de ville débutant par un caractère donné
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print("Entrez un caractère: ");
+		var caractere = lectureClavier.next().charAt(0);
+		System.out.println("Le caractère entré est : " + caractere + "\n");
+		villes.stream()
+			.filter(ville -> ville.startsWith(String.valueOf(
+							Character.toUpperCase(caractere))))
+			.forEach(ville -> {
+				nbVillesDebutantParCaractere++;
+				System.out.println(" " + ville);
+			});
+		System.out.println("\n Le nombre de villes débutant par " + 
+				caractere + " est : " + nbVillesDebutantParCaractere);
 
 
 	}
